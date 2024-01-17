69260c0fafc55b61fd0007329a94e814876b5fcf
CYBEREYE\chung.nguyenhuynh <CET@2023!>
2024-01-17 18:01:35 +0700

    cmit

--- WebDuLich/WebDuLich/Content/js/Cus.js
+++ WebDuLich/WebDuLich/Content/js/Cus.js
@@ -4,3 +4,28 @@ function handlerConfirm(e) {
         location.href = e
     }
 }
+
+
+$(function () {
+    // turn the element to select2 select style
+    $('.select2').on('change', function () {
+        var data = $(".select2 option:selected").val();
+        switch (data) {
+            case "1":
+                $("#Code").val("OT-")
+                break;
+            case "2":
+                $("#Code").val("Xm-")
+                break;
+            case "3":
+                $("#Code").val("MB-")
+                break;
+            case "4":
+                $("#Code").val("TH-")
+                break;
+            default:
+                break;
+        }
+        
+    })
+});
